53d5ef6df38c6becc8ad4ebc9d670c6c63fbef73
RocioRojas <rociorojas391@gmail.com>
2021-02-11 11:08:31 -0400

    agrego información importante desde la API, colores e imagenes

--- cryptoTracker/src/components/coins/CoinsScreen.js
+++ cryptoTracker/src/components/coins/CoinsScreen.js
@@ -1,26 +1,56 @@
 import React, {Component} from 'react';
-import {View, Text, Pressable, StyleSheet} from 'react-native';
-
+import {
+  View,
+  Text,
+  Pressable,
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+} from 'react-native';
+import Http from 'cryptoTracker/src/libs/http';
+import CoinsItem from './CoinsItem';
+import colors from 'cryptoTracker/src/res/colors';
 class CoinsScreen extends Component {
+  state = {
+    coins: [],
+    loading: false,
+  };
+  componentDidMount = async () => {
+    this.setState({loading: true});
+    const res = await Http.instance.get(
+      'https://api.coinlore.net/api/tickers/',
+    );
+    this.setState({coins: res.data, loading: false});
+  };
+
   handlePress = () => {
+    console.log('Acabo de presionar el boton');
     console.log('go to detail', this.props);
+    console.log('Ver prop', this);
+    console.log('Ver prop', this.props.route.name);
     this.props.navigation.navigate('CoinDetail');
   };
+
   render() {
+    const {coins, loading} = this.state;
     return (
       <View style={Styles.container}>
-        <Text style={Styles.titleText}>Coins Screen</Text>
-        <Pressable style={Styles.btn} onPress={this.handlePress}>
-          <Text style={Styles.btnText}> Ir a detail </Text>
-        </Pressable>
+        {loading ? (
+          <ActivityIndicator color="#fff" size="large" style={Styles.loader} />
+        ) : null}
+        <FlatList
+          data={coins}
+          renderItem={({item}) => <CoinsItem item={item} />}
+        />
       </View>
     );
   }
 }
+
 const Styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red',
+    backgroundColor: colors.charade,
   },
   titleText: {
     color: '#fff',
@@ -36,5 +66,9 @@ const Styles = StyleSheet.create({
     color: '#fff',
     textAlign: 'center',
   },
+  loader: {
+    marginTop: 80,
+  },
 });
+
 export default CoinsScreen;
--- cryptoTracker/src/components/coins/CoinsStack.js
+++ cryptoTracker/src/components/coins/CoinsStack.js
@@ -2,14 +2,26 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import CoinsScreen from './CoinsScreen';
 import CoinDetailScreen from './CoinDetailScreen';
+import colors from 'cryptoTracker/src/res/colors';
 
 const Stack = createStackNavigator();
 
 const CoinsStack = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: colors.blackPearl,
+          shadowColor: colors.blackPearl,
+        },
+        headerTintColor: colors.white,
+      }}>
       <Stack.Screen name="Coins" component={CoinsScreen} />
-      <Stack.Screen name="CoinDetail" component={CoinDetailScreen} />
+      <Stack.Screen
+        options={{headerShown: false}}
+        name="CoinDetail"
+        component={CoinDetailScreen}
+      />
     </Stack.Navigator>
   );
 };
